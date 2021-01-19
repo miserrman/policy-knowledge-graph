3c549377c22886534dc6918a18de973cfc6e35d4
穿什么都队 <1462116324@qq.com>
2021-01-19 15:00:49 +0800

    feat-添加文章内容的实体识别

--- policy/entity.py
+++ policy/entity.py
@@ -7,6 +7,9 @@ from apriori import *
 from pyhanlp import HanLP
 import regex
 import policy_util
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def cut_sentence(paragraph):
@@ -31,6 +34,11 @@ def build_vocabulary(data: pd.DataFrame):
 
 
 def extract_entity(paragraphes):
+    """
+    从一个句子数组里提取名词词语
+    :param paragraphes:
+    :return:
+    """
     extract_term = []
     for para in paragraphes:
         sentences = cut_sentence(para)
@@ -43,7 +51,29 @@ def extract_entity(paragraphes):
                 if sege.find('n') >= 0 and sege.find('v') < 0:
                     term.append(s.word)
             extract_term.append(term)
+    extract_term = [str(term) for term in extract_term]
     return extract_term
+
+
+def tf_idf_statistic(data):
+    """
+    使用TF-IDF判断文章的词频
+    :param data:
+    :return:
+    """
+    all_word = []
+    for index, row in data.iterrows():
+        content = row['content']
+        paras = cut_sentence(content)
+        words = extract_entity(paras)
+        word = ' '.join(words)
+        all_word.append(word)
+    vectorizer = CountVectorizer()
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(vectorizer.fit_transform(all_word))
+    word = vectorizer.get_feature_names()
+    weight = tfidf.toarray()
+    return word, weight
 
 
 def entity_recognition(data: pd.DataFrame):
@@ -54,8 +84,73 @@ def entity_recognition(data: pd.DataFrame):
     term_data = extract_entity(paragraphes)
     m, n = apriori(term_data, minSupport=0.001)
     rules = generateRules(m, n, minConf=0.01)
-    # c = createC1(term_data)
     print(rules)
+
+
+def extract_high_rate_word(data: pd.DataFrame, rate):
+    """
+    提取高频词作为实体，使用tf-idf
+    :param data: 使用数据
+    :param rate: 提取率，提取词频前%n的词
+    :return:
+    """
+    word, weight = tf_idf_statistic(data)
+    result = []
+    for i in range(len(weight)):
+        res = []
+        content = dict()
+        for j in range(len(word)):
+            if weight[i][j] > 0:
+                content[word[j]] = weight[i][j]
+        length = len(content)
+        content = sorted(content.items(), key=lambda d: d[1], reverse=True)
+        t = 0
+        for key, value in content:
+            if t >= int(length * rate):
+                break
+            res.append(key)
+            t += 1
+        result.append(res)
+    entity_paras = []
+    entity_includes = []
+    for index, row in data.iterrows():
+        content = row['content']
+        res, include_entity = get_entity_para(content, result[index])
+        if index == 2:
+            print(res)
+            print(include_entity)
+        entity_paras.append(res)
+        entity_includes.append(include_entity)
+    policy_entity_res = []
+    for i in range(len(entity_paras)):
+        l = len(entity_paras[i])
+        for j in range(l):
+            entity_res = {
+                'id': i,
+                'para': entity_paras[i][j],
+                'entity': '|'.join(entity_includes[i][j])
+            }
+            policy_entity_res.append(entity_res)
+    policy_util.write_csv(policy_entity_res, 'data/result/policy_fujian_content_entity.csv')
+
+
+def get_entity_para(content, entity):
+    if not content:
+        return []
+    paras = cut_sentence(content)
+    result = []
+    include_entity = []
+    for para in paras:
+        entities = []
+        count = 0
+        for e in entity:
+            if para.find(e) >= 0:
+                entities.append(e)
+                count += 1
+        if count >= 2:
+            result.append(para)
+            include_entity.append(entities)
+    return result, include_entity
 
 
 def extract_special_policy(data: pd.DataFrame):
@@ -112,10 +207,11 @@ def dependency_analysis(sent):
     return res
 
 
-
 if __name__ == '__main__':
     # content = pd.read_csv('data/policy_content.csv', header=0)
-    # entity_recognition(content)
-    data = pd.read_csv('data/policy_fujian.csv', header=0)
-    extract_special_policy(data)
+    # entity_recognition(content)#使用关联分析方法分析
+    # data = pd.read_csv('data/policy_fujian.csv', header=0)
+    # extract_special_policy(data)#使用依存关系分析标题
+    data = pd.read_csv('data/policy_fujian_content.csv', header=0)
+    extract_high_rate_word(data, 0.1)
 
--- policy/relation.py
+++ policy/relation.py
@@ -11,9 +11,18 @@ def dependency_relation(sentence):
 
 
 if __name__ == "__main__":
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36'
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36'
+    # }
+    # t = requests.get('https://www.fujiansme.com/index.php?m=content&c=index&a=show&catid=700&id=2630', headers=headers)
+    # fp = open('3.txt', 'w')
+    # fp.write(t.text)
+    d = {
+        'a': 4,
+        'b': 7,
+        'c': 2
     }
-    t = requests.get('https://www.fujiansme.com/index.php?m=content&c=index&a=show&catid=700&id=2630', headers=headers)
-    fp = open('3.txt', 'w')
-    fp.write(t.text)
+    d = sorted(d.items(), key=lambda d:d[1], reverse=True)
+    for key, value in d:
+        print(value)
+    print(d)
